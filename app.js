18119b4bd60637a0c1afae0cbaf2f87a90ffe867
Martin Kütt <thedeadgamer101@gmail.com>
2022-09-05 01:10:11 -0700

    Commented a few stuff

--- app.js
+++ app.js
@@ -11,28 +11,37 @@ while (i < button_count) {
 }
 
 var selected_cells = []
+
 // Initialize socket and stuff
 const socket = io.connect('http://localhost:3310')
 
+// on connected to server
 socket.on('connect', () => {
     console.log("Has connected to socket")
 
+    // on received cells_changed
     socket.on('cells_changed', data => {
+        // import data and update it
         console.log(data)
         selected_cells = data
         refresh_cells()
     })
 
+    // emit a message test message
     socket.emit('message', 'test')
 })
 
+// on cell button pressed
 function buttonPressed(e) {
     console.log(e.id)
+    // check if socket connected
     if (socket.connected) {
+        // emit cell change
         socket.emit('cell_selected', e.id)
     }    
 }
 
+// refresh the cells accordingly
 function refresh_cells() {
     selected_cells.forEach(id => {
         document.getElementById(id).classList.remove('not_active')
--- server/index.js
+++ server/index.js
@@ -1,29 +1,40 @@
+// prequisitions
 var express = require('express');
 
 var app = express();
 var server = app.listen(3310);
 
+// set static to public doesn't exist right now
 app.use(express.static('public'));
 
+// log server
 console.log("Server running at http://localhost:3310");
 
+// websocket using io
 const io = require("socket.io")(server, {cors: { origin: "*"}})
 
+// the selected cells
 var selected_cells = []
 
+// on user/socket connection
 io.on('connection', socket => {
     console.log("A new client has connected");
+    // emit current cells
     io.emit('cells_changed', selected_cells)
 
+    // on socket message receive
     socket.on('message', data => {
         console.log(data)
     })
 
+    // on socket cell_selected receive
     socket.on('cell_selected', id => {
         console.log(id)
-    
+
+        // check if already selected if not then add to selected
         if (!selected_cells.includes(id)) selected_cells.push(id);
 
+        // emit that cells have changed
         io.emit('cells_changed', selected_cells)
     })
 });
